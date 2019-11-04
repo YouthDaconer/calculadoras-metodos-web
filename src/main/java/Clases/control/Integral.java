5822a5b3c1627497700cba290d387bd06c01b229
JSRA <JSRA@REYES-PC>
2019-11-04 10:11:36 -0500

    Corregidos unos errores en la clase Polinomio e  Integral. Funcion: método de redondeo es estatico Se separó la clase Termino de Polinomio se agregó la clase ajuste de curva

--- src/main/java/Clases/control/Integral.java
+++ src/main/java/Clases/control/Integral.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Clases.control;
 
 import java.util.Random;
@@ -320,7 +315,7 @@ public class Integral {
             double yRandom;
             this.puntos = puntos;
 
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < puntos; i++) {
                 xRandom = getRandom();
                 yRandom = max * Math.random();
                 if (yRandom <= f.f(xRandom)) {
